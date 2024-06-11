df90ab6ac6c1c4839ce25ef221dd7009154921c9
Jacob Cheatley <jacobcheatley@gmail.com>
2024-06-11 20:15:24 +1200

    Fix page function types

--- convergence_games/app/common.py
+++ convergence_games/app/common.py
@@ -3,14 +3,18 @@ from fastui import components as c
 from fastui.events import GoToEvent
 
 
-def page(*components, title: str | None = None) -> FastUI:
+def page(*components: list[c.AnyComponent], title: str | None = None) -> FastUI:
     return [
         c.PageTitle(text=f"Convergence - {title}" if title else "Convergence"),
         c.Navbar(
             title="Convergence",
             title_event=GoToEvent(url="/"),
             start_links=[
-                c.Link(components=[c.Text(text="Games")], on_click=GoToEvent(url="/games"), active="startswith:/games"),
+                c.Link(
+                    components=[c.Text(text="Games")],
+                    on_click=GoToEvent(url="/games"),
+                    active="startswith:/games",
+                ),
                 c.Link(
                     components=[c.Text(text="Sessions")],
                     on_click=GoToEvent(url="/sessions"),
