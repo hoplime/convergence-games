251dae2e204ab0c94b1371744421232f4afc7aa9
Jacob Cheatley <jacobcheatley@gmail.com>
2024-08-14 22:20:56 +1200

    Also push dev when pushing latest

--- docker_build_and_push.sh
+++ docker_build_and_push.sh
@@ -1,2 +1,4 @@
 docker build -t jcheatley/waikato-rpg-convergence-games:latest . && \
-docker push jcheatley/waikato-rpg-convergence-games:latest
+docker tag jcheatley/waikato-rpg-convergence-games:latest jcheatley/waikato-rpg-convergence-games:dev && \
+docker push jcheatley/waikato-rpg-convergence-games:latest && \
+docker push jcheatley/waikato-rpg-convergence-games:dev
