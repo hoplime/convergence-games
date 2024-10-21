2946f3acd1b23c10dde29e6af1386c61571d19ff
Jacob Cheatley <jacobcheatley@gmail.com>
2024-10-21 21:35:59 +1300

    Add favicons

--- convergence_games/app/routers/frontend/__init__.py
+++ convergence_games/app/routers/frontend/__init__.py
@@ -1,12 +1,14 @@
 from litestar.router import Router
 
+from .favicon import favicon_router
 from .home import HomeController
 from .static import static_files_router
 
 router = Router(
     path="/",
     route_handlers=[
-        static_files_router,
+        favicon_router,
         HomeController,
+        static_files_router,
     ],
 )
---
+++ convergence_games/app/routers/frontend/favicon.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from litestar import Router, get
+from litestar.config.response_cache import CACHE_FOREVER
+from litestar.handlers import HTTPRouteHandler
+from litestar.response.file import File
+
+from convergence_games.app.paths import STATIC_DIR_PATH
+
+route_handlers: list[HTTPRouteHandler] = []
+
+for favicon_path in (STATIC_DIR_PATH / "favicon").iterdir():
+    # We've got to bind favicon_file to the closure to avoid a late binding issue.
+    def create_favicon_route(favicon_path: Path = favicon_path) -> HTTPRouteHandler:
+        @get(path=f"/{favicon_path.name}", cache=CACHE_FOREVER)
+        async def favicon_route() -> File:
+            return File(favicon_path)
+
+        return favicon_route
+
+    route_handlers.append(create_favicon_route())
+
+favicon_router = Router(path="/", route_handlers=route_handlers)
