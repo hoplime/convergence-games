42ec425e290c9e8c76c3e4409df15bd83206aa56
Jacob Cheatley <jacobcheatley@gmail.com>
2024-10-22 01:16:09 +1300

    Set Vary response header

--- convergence_games/app/routers/frontend/home.py
+++ convergence_games/app/routers/frontend/home.py
@@ -5,6 +5,8 @@ from convergence_games.app.response_type import HTMXBlockTemplate, Template
 
 
 class HomeController(Controller):
+    response_headers = {"Vary": "hx-target"}
+
     @get(path=["/"])
     async def home(self, request: Request) -> Template:
         return HTMXBlockTemplate(template_name="pages/home.html.jinja", block_name=request.htmx.target)
