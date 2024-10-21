0fa6b074382021bfe2c0fe31c013a9479a8a7389
Jacob Cheatley <jacobcheatley@gmail.com>
2024-10-22 00:33:03 +1300

    Include HTMX and Hyperscript in ts build script lol

--- convergence_games/app/lib/index.ts
+++ convergence_games/app/lib/index.ts
@@ -1,5 +1,13 @@
 import { Editor } from "@tiptap/core";
 import StarterKit from "@tiptap/starter-kit";
+import _hyperscript from "hyperscript.org";
+import htmx from "htmx.org";
+
+declare global {
+    interface Window {
+        htmx: typeof htmx;
+    }
+}
 
 const createEditor = (element: Element) => {
     return new Editor({
@@ -8,5 +16,9 @@ const createEditor = (element: Element) => {
         content: "<p>Hello World!</p>",
     });
 };
+
+// I know it's ridiculous, but it's fun to bundle htmx and hyperscript with a build step instead of using a CDN
+window.htmx = htmx;
+_hyperscript.browserInit();
 
 export { createEditor };
