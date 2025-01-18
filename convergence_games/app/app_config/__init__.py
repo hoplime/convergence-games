aed19ef6b6fb84223fbc54f33ac8d2e868360a7c
Jacob Cheatley <jacobcheatley@gmail.com>
2025-01-18 23:21:32 +1300

    Remove mock_authentication_middleware

--- convergence_games/app/app_config/__init__.py
+++ convergence_games/app/app_config/__init__.py
@@ -1,7 +1,6 @@
 from .compression_config import compression_config
 from .htmx_plugin import htmx_plugin
 from .jwt_cookie_auth import jwt_cookie_auth
-from .mock_authentication_middleware import mock_authentication_middleware
 from .openapi_config import openapi_config
 from .sqlalchemy_plugin import sqlalchemy_plugin
 from .template_config import template_config
@@ -9,7 +8,6 @@ from .template_config import template_config
 __all__ = [
     "compression_config",
     "htmx_plugin",
-    "mock_authentication_middleware",
     "openapi_config",
     "sqlalchemy_plugin",
     "template_config",
