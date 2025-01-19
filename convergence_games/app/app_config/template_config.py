2d281a9a54bad42416fdc2b6e526fd04c3c470fa
Jacob Cheatley <jacobcheatley@gmail.com>
2025-01-19 14:15:29 +1300

    Template rendering cleanup

--- convergence_games/app/app_config/template_config.py
+++ convergence_games/app/app_config/template_config.py
@@ -2,6 +2,7 @@ from typing import Any
 
 import jinjax
 from jinja2 import Environment, FileSystemLoader
+from jinja2.utils import pass_context
 from litestar.contrib.jinja import JinjaTemplateEngine
 from litestar.template.config import TemplateConfig
 
@@ -21,10 +22,34 @@ def debug(text: Any) -> str:
     return ""
 
 
+@pass_context
+def blah(context, text: str) -> None:
+    print(context)
+    print(text)
+
+
+original_init = jinjax.Component.__init__
+
+
+class ComponentWithRequestPassthrough(jinjax.Component):
+    def __init__(self, *args, **kwargs):
+        original_init(self, *args, **kwargs)
+        self.optional["request"] = None
+
+
+jinjax.Component.__init__ = ComponentWithRequestPassthrough.__init__  # type: ignore
+
+
+class JinjaXWithRequestPassthrough(jinjax.JinjaX):
+    def _build_call(self, tag, attrs_list, content=""):
+        attrs_list.append(("request", "{{ request }}"))
+        return super()._build_call(tag, attrs_list, content)
+
+
 jinja_env = Environment(
     loader=FileSystemLoader(searchpath=TEMPLATES_DIR_PATH),
     extensions=[
-        jinjax.JinjaX,
+        JinjaXWithRequestPassthrough,
     ],
     trim_blocks=True,
     lstrip_blocks=True,
@@ -32,6 +57,7 @@ jinja_env = Environment(
 
 jinja_env.filters["debug"] = debug
 jinja_env.filters["extract_title"] = extract_title
+jinja_env.globals["blah"] = blah
 
 catalog = jinjax.Catalog(jinja_env=jinja_env)
 catalog.add_folder(COMPONENTS_DIR_PATH)
