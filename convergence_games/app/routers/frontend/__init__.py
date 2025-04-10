fab1a3bcda5764f9d50a6a3d85fdef48289147af
Jacob Cheatley <jacobcheatley@gmail.com>
2025-04-10 20:54:33 +1200

    Add theme selector

--- convergence_games/app/routers/frontend/__init__.py
+++ convergence_games/app/routers/frontend/__init__.py
@@ -7,6 +7,7 @@ from .games import GamesController
 from .home import HomeController
 from .oauth import OAuthController
 from .profile import ProfileController
+from .settings import SettingsController
 from .static import static_files_router
 
 router = Router(
@@ -22,6 +23,7 @@ router = Router(
         HomeController,
         OAuthController,
         ProfileController,
+        SettingsController,
         static_files_router,
     ],
 )
