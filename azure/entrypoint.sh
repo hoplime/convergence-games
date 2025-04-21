9df009495a3b2820d5a700716e919a90b946b454
Jacob Cheatley <jacobcheatley@gmail.com>
2025-04-21 21:09:15 +1200

    Add DEBUG_SERVER_CONFIG flag for testing ssh and not crashing

--- azure/entrypoint.sh
+++ azure/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
 set -e
 service ssh start
-# We pass the command as CMD in the Dockerfile
-exec "$@"
+if [[ -z "$DEBUG_SERVER_CONFIG" ]]; then
+    # We pass the command as CMD in the Dockerfile
+    exec "$@"
+else
+    # We just run a simple HTTP server, not the one in the container
+    # This is useful for debugging the container
+    # and for testing the SSH connection
+    exec python -m http.server
+fi
