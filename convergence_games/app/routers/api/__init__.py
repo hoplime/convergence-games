1b685846d2e26169fb964c371a1a4e81328501c9
Jacob Cheatley <jacobcheatley@gmail.com>
2025-04-29 19:35:33 +1200

    Don't expose create_mock_data without debug enabled

--- convergence_games/app/routers/api/__init__.py
+++ convergence_games/app/routers/api/__init__.py
@@ -1,11 +1,16 @@
 from litestar.router import Router
 
+from convergence_games.settings import SETTINGS
+
 from .debug import DebugController
+
+route_handlers = []
+
+if SETTINGS.DEBUG:
+    route_handlers.append(DebugController)
 
 router = Router(
     path="/api",
     tags=["api"],
-    route_handlers=[
-        DebugController,
-    ],
+    route_handlers=route_handlers,
 )
