2dae9bdb8205087896ff01e7a81305ed76879c08
Jacob Cheatley <jacobcheatley@gmail.com>
2025-08-15 20:15:35 +1200

    Switch alerts_response over to using exceptions

--- convergence_games/app/alerts.py
+++ convergence_games/app/alerts.py
@@ -1,9 +1,5 @@
 from dataclasses import dataclass
-from typing import Literal
-
-from convergence_games.app.app_config.template_config import catalog
-from convergence_games.app.request_type import Request
-from convergence_games.app.response_type import HTMXBlockTemplate
+from typing import Literal, final
 
 
 @dataclass
@@ -12,20 +8,10 @@ class Alert:
     message: str
 
 
-def alerts_response(alerts: list[Alert], request: Request | None = None) -> HTMXBlockTemplate:
-    """
-    Generates an HTMX response with a list of alerts.
-
-    Args:
-        alerts: A list of Alert objects to be displayed.
-        request: The request object containing user information.
-
-    Returns:
-        An HTMXBlockTemplate containing the rendered alerts.
-    """
-    template_str = catalog.render("ToastAlerts", alerts=alerts)
-    return HTMXBlockTemplate(
-        template_str=template_str,
-        re_target=request.query_params.get("alert-retarget", "#content") if request is not None else "#content",
-        re_swap="beforeend",
-    )
+@final
+class AlertError(Exception):
+    def __init__(self, alerts: list[Alert], redirect_text: str | None = None, redirect_url: str | None = None):
+        super().__init__()
+        self.alerts = alerts
+        self.redirect_text = redirect_text
+        self.redirect_url = redirect_url
--- convergence_games/app/app_config/exception_handlers.py
+++ convergence_games/app/app_config/exception_handlers.py
@@ -2,16 +2,34 @@ from litestar import Response
 from litestar.datastructures import Cookie
 from litestar.response import Redirect
 
+from convergence_games.app.alerts import AlertError
+from convergence_games.app.app_config.template_config import catalog
 from convergence_games.app.exceptions import UserNotLoggedInError
 from convergence_games.app.request_type import Request
+from convergence_games.app.response_type import HTMXBlockTemplate
 
 
-def user_not_logged_in_handler(request: Request, exc: UserNotLoggedInError) -> Response:
+def user_not_logged_in_handler(request: Request, exc: UserNotLoggedInError) -> Redirect:
     """Handle UserNotLoggedInError by redirecting to the login page."""
     request_url_path = request.scope["path"]
     return Redirect(path="/profile", cookies=[Cookie(key="invalid-action-path", value=request_url_path, max_age=30)])
 
 
+def alert_handler(request: Request, exc: AlertError) -> HTMXBlockTemplate:
+    template_str = catalog.render(
+        "ToastAlerts",
+        alerts=exc.alerts,
+        redirect_text=(exc.redirect_text or "Return Home") if not request.htmx else "",
+        redirect_url=(exc.redirect_url or "/") if not request.htmx else "",
+    )
+    return HTMXBlockTemplate(
+        template_str=template_str,
+        re_target=request.query_params.get("alert-retarget", "#content"),
+        re_swap="beforeend",
+    )
+
+
 exception_handlers = {
     UserNotLoggedInError: user_not_logged_in_handler,
+    AlertError: alert_handler,
 }
