cfad7fd52788aef18d0b6f1def1d427a06f0e354
Jacob Cheatley <jacobcheatley@gmail.com>
2025-09-05 21:01:01 +1200

    Update player count displays

--- convergence_games/app/templates/pages/event_manage_allocation.ts
+++ convergence_games/app/templates/pages/event_manage_allocation.ts
@@ -16,10 +16,12 @@ type Party = HTMLElement & {
 type SessionSlot = HTMLElement & {
     current_players: number;
     min_players: number;
+    opt_players: number;
     max_players: number;
     session: SessionID;
     session_member_list: SessionMemberList;
     session_member_count: SessionMemberCount;
+    session_member_count_displays: NodeListOf<HTMLElement>;
 };
 
 type SessionMemberList = HTMLElement & {
@@ -32,6 +34,30 @@ const is_session_member_list = (element: HTMLElement): element is SessionMemberL
 };
 
 type SessionMemberCount = HTMLElement;
+
+const update_session_member_count_display = (sessionSlot: SessionSlot) => {
+    sessionSlot.session_member_count.textContent = `${sessionSlot.current_players}/${sessionSlot.min_players}-${sessionSlot.max_players}`;
+    if (
+        sessionSlot.current_players < sessionSlot.min_players ||
+        sessionSlot.current_players > sessionSlot.max_players
+    ) {
+        sessionSlot.session_member_count.classList.add("text-warning");
+        sessionSlot.session_member_count.classList.remove("text-success");
+    } else {
+        sessionSlot.session_member_count.classList.remove("text-warning");
+        sessionSlot.session_member_count.classList.add("text-success");
+    }
+
+    for (let i = 0; i < sessionSlot.session_member_count_displays.length; i++) {
+        const display = sessionSlot.session_member_count_displays[i];
+        console.log(display);
+        if (i < sessionSlot.current_players) {
+            display.classList.add("enabled");
+        } else {
+            display.classList.remove("enabled");
+        }
+    }
+};
 
 const event_manage_allocation = (scope_id: string) => {
     const scope = document.getElementById(scope_id);
@@ -49,7 +75,6 @@ const event_manage_allocation = (scope_id: string) => {
         party: Party,
         fromElement: SessionMemberList | HTMLElement,
         toElement: SessionMemberList | HTMLElement,
-        evt: Sortable.SortableEvent | null = null,
     ) => {
         console.log("Emplacing party");
         console.log(party);
@@ -57,21 +82,32 @@ const event_manage_allocation = (scope_id: string) => {
         // Deal with FROM
         if (is_session_member_list(fromElement)) {
             const sessionSlot = fromElement.session_slot;
-            sessionSlot.current_players -= party.member_count;
-            sessionSlot.session_member_count.textContent = `${sessionSlot.current_players}/${sessionSlot.min_players}-${sessionSlot.max_players}`;
+
+            if (party.gm_of.includes(sessionSlot.session)) {
+                // We were the GM of the session we just left
+            } else {
+                // We were a player of the session we just left
+                sessionSlot.current_players -= party.member_count;
+                update_session_member_count_display(sessionSlot);
+            }
         }
 
         // Deal with TO
         if (is_session_member_list(toElement)) {
             const sessionSlot = toElement.session_slot;
-            sessionSlot.current_players += party.member_count;
-            sessionSlot.session_member_count.textContent = `${sessionSlot.current_players}/${sessionSlot.min_players}-${sessionSlot.max_players}`;
-            party.dataset.tierValue = party.tiers[sessionSlot.session || ""]?.tier.toString() || "unknown";
 
             // If we are the GM, move to the top of the list
             // It is already in place so just move it to the front
             if (party.gm_of.includes(sessionSlot.session)) {
+                // We are the GM of this session
+                party.dataset.inGmGame = "true";
                 sessionSlot.session_member_list.prepend(party);
+            } else {
+                // We are a player of this session
+                sessionSlot.current_players += party.member_count;
+                update_session_member_count_display(sessionSlot);
+                delete party.dataset.inGmGame;
+                party.dataset.tierValue = party.tiers[sessionSlot.session || ""]?.tier.toString() || "unknown";
             }
         }
     };
@@ -98,15 +134,18 @@ const event_manage_allocation = (scope_id: string) => {
     for (const sessionSlot of sessionSlotElements) {
         const sessionMemberList = sessionSlot.querySelector(".session-member-list") as SessionMemberList;
         const sessionMemberCount = sessionSlot.querySelector(".session-member-count") as SessionMemberCount;
+        const sessionMemberCountDisplay = sessionSlot.querySelector(".session-member-count-display") as HTMLElement;
 
         Sortable.create(sessionMemberList, options);
 
         sessionSlot.current_players = 0;
         sessionSlot.min_players = parseInt(sessionSlot.dataset.minPlayers || "0", 10);
+        sessionSlot.opt_players = parseInt(sessionSlot.dataset.optPlayers || "0", 10);
         sessionSlot.max_players = parseInt(sessionSlot.dataset.maxPlayers || "0", 10);
         sessionSlot.session = sessionSlot.dataset.session || "";
         sessionSlot.session_member_list = sessionMemberList;
         sessionSlot.session_member_count = sessionMemberCount;
+        sessionSlot.session_member_count_displays = sessionMemberCountDisplay.querySelectorAll("div");
 
         sessionMemberList.typ = "member-list";
         sessionMemberList.session_slot = sessionSlot;
