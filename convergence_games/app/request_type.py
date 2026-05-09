3c41cbc25ecea89003282ea313978b3046356b6c
Jacob Cheatley <jacobcheatley@gmail.com>
2026-05-09 17:55:23 +1200

    Slightly improve type checking

--- convergence_games/app/request_type.py
+++ convergence_games/app/request_type.py
@@ -1,38 +1,65 @@
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, TypeAlias
+from typing import TYPE_CHECKING, Any, Literal, TypeAlias, override
 
-from litestar.datastructures import State
+from litestar.connection import ASGIConnection
 from litestar.security.jwt import Token
 
-from convergence_games.db.models import User
-
 # This file is responsible for exporting the following types:
-# - Token: The type of the token object for the JWT cookie authentication.
+# - CustomToken: The type of the token object for the JWT cookie authentication.
 # - Request: The type of the request object for handlers and middleware.
-# - ASGIConnection: The type of the connection object for handlers and middleware.
+# - TypedASGIConnection: The type of the connection object for handlers and middleware.
+
+type TokenType = Literal["legacy", "access", "refresh"]
 
 
 @dataclass
 class CustomToken(Token):
-    flag: bool = False
+    token_type: TokenType = "legacy"
 
+
+type AnyASGIConnection = ASGIConnection[Any, Any, Any, Any]  # pyright: ignore[reportExplicitAny]
 
 if TYPE_CHECKING:
+    from litestar import Litestar
     from litestar import Request as _Request
-    from litestar.connection import ASGIConnection as _ASGIConnection
+    from litestar.datastructures import Cookie, State
     from litestar.handlers.http_handlers import HTTPRouteHandler
     from litestar.plugins.htmx import HTMXDetails
+    from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker
+
+    from convergence_games.db.models import User
+
+    class AppState(State):
+        db_engine: AsyncEngine
+        session_maker_class: async_sessionmaker[AsyncSession]
+
+    class RequestState(State):
+        pending_auth_cookies: list[Cookie]
+        current_session_jti: str | None
+
+    class AppWithState(Litestar):
+        state: AppState
 
     # This is a workaround for the HTMXRequest not being a generic type, and us wanting to annotate the User type.
-    class HTMXRequest(_Request[User | None, CustomToken, State]):
-        htmx: HTMXDetails
+    class HTMXRequest(_Request[User | None, CustomToken, RequestState]):
+        htmx: HTMXDetails  # pyright: ignore[reportUninitializedInstanceVariable]
 
-    class ASGIConnection(_ASGIConnection[HTTPRouteHandler, User | None, CustomToken, State]):
-        pass
+        @property
+        @override
+        def app(self) -> AppWithState: ...
+
+    class TypedASGIConnection(ASGIConnection[HTTPRouteHandler, User | None, CustomToken, RequestState]):
+        @property
+        @override
+        def app(self) -> AppWithState: ...
 
     Request: TypeAlias = HTMXRequest
 else:
-    from litestar.connection import ASGIConnection as ASGIConnection
+    from litestar.connection import ASGIConnection
     from litestar.plugins.htmx import HTMXRequest
 
+    AppState = State
+    RequestState = State
+    AppWithState = Litestar
+    TypedASGIConnection = ASGIConnection
     Request: TypeAlias = HTMXRequest
