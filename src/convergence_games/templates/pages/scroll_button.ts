3f4ea4749ead57910f41a5401cc5b9df8287234d
Andrew Mackintosh <5974998+null-cipher@users.noreply.github.com>
2026-08-21 22:31:52 +1200

    Hide scroll button until futher down the page
    The scroll to top button now starts hidden and is shown once the nav
    bar scrolls out of the viewport. The button is hidden again when at
    the top of the page.

--- src/convergence_games/templates/pages/scroll_button.ts
+++ src/convergence_games/templates/pages/scroll_button.ts
@@ -5,6 +5,21 @@ const scroll_button = (element_id: string) => {
             console.error(`Could not find ${element_id} element`);
             return;
         }
+        // Create an observer that watches the nav element. When it scrolls
+        // out of the viewport (by more than 200px), we unhide the scroll to
+        // top button.
+        const observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
+            entries.forEach((entry: IntersectionObserverEntry) => {
+                if (entry.isIntersecting) {
+                    button.classList.add("hidden");
+                } else {
+                    button.classList.remove("hidden");
+                }
+            });
+        }, {
+            rootMargin: "200px"
+        });
+        observer.observe(document.getElementsByTagName("nav")[0]);
 
         button.addEventListener('click', () => {
             document.documentElement.scrollTo({
